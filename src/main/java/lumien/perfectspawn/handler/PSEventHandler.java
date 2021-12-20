2fc4adeeb990ab4cc067d978c7038ab6c15f3048
HiSkyZen <mchan030704@gmail.com>
2021-12-21 00:02:07 +0900

    Fix Build Error

--- src/main/java/lumien/perfectspawn/handler/PSEventHandler.java
+++ src/main/java/lumien/perfectspawn/handler/PSEventHandler.java
@@ -102,10 +102,14 @@ public class PSEventHandler
 		ReflectionHandler.spawnShoulderEntities(player);
 		ReflectionHandler.setSize(player, 0.2F, 0.2F);
 
+
+
 		if (enumfacing != null)
 		{
-			float f1 = 0.5F + (float) enumfacing.getFrontOffsetX() * 0.4F;
-			float f = 0.5F + (float) enumfacing.getFrontOffsetZ() * 0.4F;
+			new getFOXonEF() = enumfacing.getFrontOffsetX()
+			new getFOZonEF() = enumfacing.getFrontOffsetZ()
+			float f1 = 0.5F + (float) getFOXonEF() * 0.4F;
+			float f = 0.5F + (float) getFOZonEF() * 0.4F;
 
 			ReflectionHandler.setRenderOffsetForSleep(player, enumfacing);
 			player.setPosition((double) ((float) event.getPos().getX() + f1), (double) ((float) event.getPos().getY() + 0.6875F), (double) ((float) event.getPos().getZ() + f));
